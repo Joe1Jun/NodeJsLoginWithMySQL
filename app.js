7d775e1c4cbff38b1cc53705549cfaaa348f34bf
Joe1Jun <junker91joe@gmail.com>
2024-07-13 12:30:49 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const path = require("path");
+const mysql2 = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config({
+    path: './.env'
+
+});
+
+const app = express();
+
+
+const database = mysql2.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "E-COMMERCE"// Use the DATABASE environment variable for the database name
+});
+
+
+const publicDirectory = path.join(__dirname, './public')
+app.use(express.static(publicDirectory));
+
+
+//parse url encoded bodies as sent by HTML forms
+app.use(express.urlencoded({ extended: false }));
+//Parse JSON bodies as sent by API clients
+
+app.use(express.json());
+
+app.set('view engine', 'hbs');
+
+
+database.connect((error) =>{
+
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("MYSQL Connected....")
+    }
+}
+)
+//define routes
+
+app.use('/', require("./routes/pages"));
+app.use("/auth", require("./routes/auth"));
+
+
+
+app.listen(5000, () => {
+
+    console.log("Server started on Port 5000");
+})
+
+
