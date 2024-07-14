37a34daefbbfc30783f9e04f047efe46b06fa119
Joe1Jun <junker91joe@gmail.com>
2024-07-14 11:05:50 +0100

    Added hashed password functiionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,73 +1,56 @@
-
 const mysql2 = require("mysql2");
 const jwt = require("jsonwebtoken");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+
+
 
 const database = mysql2.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "nodejs-login"// Use the DATABASE environment variable for the database name
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE // Use the DATABASE environment variable for the database name
 });
-
-
-database.connect((error) =>{
-
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("MYSQL Connected....")
-    }
-}
-)
-
 
 
 exports.register = (req, res) => {
     
     //console out the request of the form
+    
     console.log(req.body);
+    
+
     const { name, email, password, passwordConfirm } = req.body;
 
-    database.query('SELECT email FROM users WHERE email = ?', [email], async (error, results) => {
+    database.query('SELECT email FROM users WHERE email = ?',  [email], async (error, results) => {
+
         if (error) {
-            console.error("Error executing database query:", error);
-            return res.status(500).send("An error occurred while processing your request.");
+            
+            console.log(error);
         }
         if (results.length > 0) {
-            
-            return res.render('register', {
-
-                message: 'That email is already in use'
+            return res.render("register", {
+                message: "That email is already in use"
             })
         } else if (password !== passwordConfirm) {
-            return res.render('register', {
-
-                message: 'Passwords do not match'
-            })
-
+            return res.render("register", {
+                message: "Passwords do not match"
+            });
+        
         }
-
         let hashedPassword = await bcrypt.hash(password, 8);
         console.log(hashedPassword);
 
-        //first one is name of database the second is the name from the form
-        
-        database.query('INSERT INTO users SET ?', { name: name, email: email, password: hashedPassword, }, (error, results) => {
-            
-            if (error) {
-                console.log(error);
+        res.send("testing")
+    });
 
-            } else {
-                console.log(results);
-                return res.render('register', {
-                    
-                    message: 'User registered'
-                })
-           }
 
-        })
-    })
+
+
+    
+    
+
+    
+  
           
 }
 
