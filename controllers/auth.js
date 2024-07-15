2c6f8e5ca31979c150dbc4394e3fb125a166545e
Joe1Jun <junker91joe@gmail.com>
2024-07-15 14:00:20 +0100

    Implemented the  middleware to check if the user is authenticated using a JWT cookie.Verified the JWT token to extract user ID.Performed a database query to retrieve the user information based on the decoded ID.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,111 +1,132 @@
-const mysql2 = require("mysql2");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+const mysql = require("mysql");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { promisify } = require('util');
 
-
-
-const database = mysql2.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE // Use the DATABASE environment variable for the database name
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE
 });
 
 exports.login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-        if (!email || !password) {
-            return res.status(400).render("login", {
-                message: "Please provide an email and password"
-            })
-        }
-
-        database.query('SELECT * FROM users WHERE email = ?', [email], async (error, results) => {
-            console.log(results);
-            if (!results || !(await bcrypt.compare(password, results[0].password))){
-                res.status(401).render("login", {
-                    message: "Email or Password is incorrect"
-                })
-            } else {
-                const id = results[0].id;
-
-                const token = jwt.sign({ id: id }, process.env.JWT_SECRET, {
-                    expiresIn: process.env.JWT_EXPIRES_IN
-                });
-                console.log("The token is " + token);
-
-                const cookieOptions = {
-                    expires: new Date(
-                        Date.now ()+ process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
-                    ),
-                    httpOnly: true
-                }
-                res.cookie('jwt', token, cookieOptions);
-                res.status(200).redirect("/");
-            }
-        })
-        
-    } catch (error) {
-        console.log(error);
+    if( !email || !password ) {
+      return res.status(400).render('login', {
+        message: 'Please provide an email and password'
+      })
     }
 
-}
+    db.query('SELECT * FROM users WHERE email = ?', [email], async (error, results) => {
+      console.log(results);
+      if( !results || !(await bcrypt.compare(password, results[0].password)) ) {
+        res.status(401).render('login', {
+          message: 'Email or Password is incorrect'
+        })
+      } else {
+        const id = results[0].id;
 
+        const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+          expiresIn: process.env.JWT_EXPIRES_IN
+        });
+
+        console.log("The token is: " + token);
+
+        const cookieOptions = {
+          expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+          ),
+          httpOnly: true
+        }
+
+        res.cookie('jwt', token, cookieOptions );
+        res.status(200).redirect("/");
+      }
+
+    })
+
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 exports.register = (req, res) => {
-    
-    //console out the request of the form
-    
-    console.log(req.body);
-    
+  console.log(req.body);
 
-    const { name, email, password, passwordConfirm } = req.body;
+  const { name, email, password, passwordConfirm } = req.body;
 
-    database.query('SELECT email FROM users WHERE email = ?',  [email], async (error, results) => {
+  db.query('SELECT email FROM users WHERE email = ?', [email], async (error, results) => {
+    if(error) {
+      console.log(error);
+    }
 
-        if (error) {
-            
-            console.log(error);
-        }
-        if (results.length > 0) {
-            return res.render("register", {
-                message: "That email is already in use"
-            })
-        } else if (password !== passwordConfirm) {
-            return res.render("register", {
-                message: "Passwords do not match"
-            });
-        
-        }
-        let hashedPassword = await bcrypt.hash(password, 8);
-        console.log(hashedPassword);
+    if( results.length > 0 ) {
+      return res.render('register', {
+        message: 'That email is already in use'
+      })
+    } else if( password !== passwordConfirm ) {
+      return res.render('register', {
+        message: 'Passwords do not match'
+      });
+    }
 
-        database.query('INSERT INTO users SET ?', { name: name, email: email, password: hashedPassword }, (error, results) => {
+    let hashedPassword = await bcrypt.hash(password, 8);
+    console.log(hashedPassword);
 
-            if (error) {
-                console.log(error);
-            }
-            else{
-                return res.render("register", {
-                    message: "User registered"
-                });
-            }
-        })
-        
-    });
-
-    
+    db.query('INSERT INTO users SET ?', {name: name, email: email, password: hashedPassword }, (error, results) => {
+      if(error) {
+        console.log(error);
+      } else {
+        console.log(results);
+        return res.render('register', {
+          message: 'User registered'
+        });
+      }
+    })
 
 
+  });
 
-
-    
-    
-
-    
-  
-          
 }
 
-   
+exports.isLoggedIn = async (req, res, next) => {
+  // console.log(req.cookies);
+  if( req.cookies.jwt) {
+    try {
+      //1) verify the token
+      const decoded = await promisify(jwt.verify)(req.cookies.jwt,
+      process.env.JWT_SECRET
+      );
+
+      console.log(decoded);
+
+      //2) Check if the user still exists
+      db.query('SELECT * FROM users WHERE id = ?', [decoded.id], (error, result) => {
+        console.log(result);
+
+        if(!result) {
+          return next();
+        }
+
+        req.user = result[0];
+         console.log("user is")
+        console.log(req.user);
+        return next();
+
+      });
+    } catch (error) {
+      console.log(error);
+      return next();
+    }
+  } else {
+    next();
+  }
+}
+
+
+  
+  
+
--- routes/pages.js
+++ routes/pages.js
@@ -1,13 +1,17 @@
 
 const express = require("express");
+const authController = require("../controllers/auth.js");
 const router = express.Router();
 
 
 
 
-router.get("/", (req, res) => {
+router.get("/", authController.isLoggedIn, (req, res) => {
 
-    res.render("index");
+    
+    res.render("index", {
+        user: req.user
+    });
 })
 
 router.get("/register", (req, res) => {
@@ -22,9 +26,20 @@ router.get("/login", (req, res) => {
     res.render("login");
 })
 
-router.get("/profile", (req, res) => {
-
-    res.render("profile");
+//middleware importing file from auth.js and using the function
+// next function makes sure page is rendered
+router.get("/profile", authController.isLoggedIn, (req, res) => {
+    if (req.user) {
+        
+        res.render("profile", {
+            user: req.user
+        });
+       
+    }
+    else {
+        
+        res.redirect("/login")
+    }
 })
 
 module.exports = router;
