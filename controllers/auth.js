50ada0f126a50671f894d103bfa77fb82a8995c9
Joe1Jun <junker91joe@gmail.com>
2024-07-15 14:16:13 +0100

    feat(auth): Implement user logout functionality by invalidating JWT cookie and redirecting to homepage.

--- controllers/auth.js
+++ controllers/auth.js
@@ -126,7 +126,17 @@ exports.isLoggedIn = async (req, res, next) => {
   }
 }
 
+exports.logout = async (req, res) => {
 
+  res.cookie('jwt', 'logout', {
+
+    expires: new Date(Date.now() + 2 * 1000),
+    httpOnly: true
+  });
+
+
+  res.status(200).redirect('/')
+}
   
   
 
